79eba8b8346045f5531255b7e075568305728e16
Liu (Zuguang Liu) <55007630+liu2z2@users.noreply.github.com>
2021-04-24 20:59:51 -0400

    Fix issue where movement policy is not called (#26)
    Also Adjust movement policy to be more realistic

--- src/agent.py
+++ src/agent.py
@@ -68,74 +68,19 @@ class Agent(ABC):
         self._alive = True
         self._color = None
         self._movement_policy = movement_policy
-
     @abstractmethod
     def get_move(self, neighbors: list[Agent],
                  thermal_points: dict[str, float]) -> np.ndarray[int]:
         """Calculate the current move given the neighbors.
-
         Parameters
         ----------
         neighbors : list[Agent]
             List of neighbors within sense_radius
-        thermal_points : dict of {str: np.ndarray}
-            Dictionary of thermal profile in four directions
-
         Returns
         np.ndarray[int]
             Agent's move in the form (row, column)
         """
-
-        # Nothing to target when there is no neighors sensed
-        if (len(neighbors)==0):
-            return np.array(self.position)
-
-        # Collect relative position of neighbors
-        neighbors_rpos = np.stack([n.position - self.position
-                                  for n in neighbors])
-        if self._movement_policy == "average":
-            target_pos = np.sum(neighbors_rpos, axis=0)
-            # Average policy takes means of all agent positions, set as target
-        elif self._movement_policy == "closest":
-            best_neighbor_i = np.argmin(
-                np.abs(neighbors_rpos).sum(axis=1))
-            target_pos = neighbors_rpos[best_neighbor_i]
-            # Closest policy target the closest agent
-
-        # Decide to move toward/away from target, or stay in place
-        if self.core_temp < self._low_move_threshold:
-            target_pos = target_pos + self.position
-        elif self.core_temp > self._high_move_threshold:
-            target_pos = -1*target_pos + self.position
-        else:
-            return np.array(self.position)
-
-        # Calculate the optimal final position closest to target
-        best_pos = np.array(self.position)
-        for step in self._movement_speed:
-            distances = [
-                abs(best_pos[0] + 0 - target_pos[0] +
-                    best_pos[1] + 0 - target_pos[1]),  # stay
-                abs(best_pos[0] + 0 - target_pos[0] +
-                    best_pos[1] + 1 - target_pos[1]),  # up
-                abs(best_pos[0] + 0 - target_pos[0] +
-                    best_pos[1] - 1 - target_pos[1]),  # down
-                abs(best_pos[0] - 1 - target_pos[0] +
-                    best_pos[1] + 0 - target_pos[1]),  # left
-                abs(best_pos[0] + 1 - target_pos[0] +
-                    best_pos[1] + 0 - target_pos[1])   # right
-            ]
-            direction = np.argmin(distances)
-            if direction == 1:
-                best_pos[1] += 1
-            elif direction == 2:
-                best_pos[1] += -1
-            elif direction == 3:
-                best_pos[0] += -1
-            elif direction == 4:
-                best_pos[0] += 1
-
-        return best_pos
+        ...
 
     @property
     def body_radius(self) -> int:
--- src/penguin.py
+++ src/penguin.py
@@ -24,4 +24,55 @@ class Penguin(Agent):
         tuple(int)
             Agent's move in the form (row, column)
         """
-        return self.position + np.random.randint(-3, 4, size=2, dtype=int)
+
+        # Nothing to target when there is no neighors sensed
+        best_pos = np.array(self.position) +np.random.randint(-3, 4, size=2, dtype=int)
+
+        if (len(neighbors)==0):
+            return best_pos
+
+        # Collect relative position of neighbors
+        neighbors_rpos = np.stack([n.position - self.position
+                                  for n in neighbors])
+        if self._movement_policy == "average":
+            target_pos = np.sum(neighbors_rpos, axis=0)
+            # Average policy takes means of all agent positions, set as target
+        elif self._movement_policy == "closest":
+            best_neighbor_i = np.argmin(
+                np.abs(neighbors_rpos).sum(axis=1))
+            target_pos = neighbors_rpos[best_neighbor_i]
+            # Closest policy target the closest agent
+
+        # Decide to move toward/away from target, or stay in place
+        if self.core_temp < self._low_move_threshold:
+            target_pos = target_pos + self.position
+        elif self.core_temp > self._high_move_threshold:
+            target_pos = -1*target_pos + self.position
+        else:
+            return best_pos
+
+        # Calculate the optimal final position closest to target
+        for step in range(self._movement_speed):
+            distances = [
+                abs(best_pos[0] + 0 - target_pos[0]) +
+                    abs(best_pos[1] + 0 - target_pos[1]),  # stay
+                abs(best_pos[0] + 0 - target_pos[0]) +
+                    abs(best_pos[1] + 1 - target_pos[1]),  # up
+                abs(best_pos[0] + 0 - target_pos[0]) +
+                    abs(best_pos[1] - 1 - target_pos[1]),  # down
+                abs(best_pos[0] - 1 - target_pos[0]) +
+                    abs(best_pos[1] + 0 - target_pos[1]),  # left
+                abs(best_pos[0] + 1 - target_pos[0]) +
+                    abs(best_pos[1] + 0 - target_pos[1])   # right
+            ]
+            direction = np.argmin(distances)
+            if direction == 1:
+                best_pos[1] += 1
+            elif direction == 2:
+                best_pos[1] += -1
+            elif direction == 3:
+                best_pos[0] += -1
+            elif direction == 4:
+                best_pos[0] += 1
+
+        return best_pos
