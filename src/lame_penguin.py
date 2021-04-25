2016f093e532fb88ee33a87e693bf98fc3dbe6be
stegnerw <wayne.stegner@protonmail.com>
2021-04-24 22:40:34 -0400

    Add thermal benchmark

---
+++ src/lame_penguin.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""This module implements the Penguin agent.
+"""
+# Standard library
+from __future__ import annotations
+# Packages
+import numpy as np
+# Custom
+from agent import Agent
+
+
+class LamePenguin(Agent):
+    """Penguin agent class."""
+    def get_move(self, neighbors: list[Agent],
+                 thermal_points: dict[str, float]) -> np.ndarray[int]:
+        """Calculate the current move given the neighbors.
+
+        Parameters
+        ----------
+        neighbors : list
+            List of neighbors within sense_radius
+
+        Returns
+        tuple(int)
+            Agent's move in the form (row, column)
+        """
+        return self.position
